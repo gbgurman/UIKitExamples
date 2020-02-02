13857e198cb1450260bd07a58463c14e44ab1a6c
gbgurman <pinkhatapps@gmail.com>
2020-02-02 20:34:07 +0300

    First commit.

---
+++ SwiftUIKitSample1/SwiftUIKitSample1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftUIKitSample1
+//
+//  Created by trin on 2.02.2020.
+//  Copyright © 2020 Pink Hat Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
